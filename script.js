61e1a2bc1328bbe1a88938eba90a9507a4578542
alexsobul <sobul-aleksei@mail.ru>
2025-11-02 19:49:22 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let sr = ScrollReveal({
+    distance: '165px',
+    duration: 1600,
+    reset:false
+})
+sr.reveal('.header-right',{delay:500, origin:'right'});
+sr.reveal('.header-icons',{delay:200, origin:'top'});
+sr.reveal('.about-container',{delay:100, origin:'left'});
+sr.reveal('.portfolio-container',{delay:100, origin:'right'});
+sr.reveal('.stages-container',{delay:100, origin:'left'});
